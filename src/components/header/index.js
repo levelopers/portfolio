93e07ddf8d40e1b3500be5cf526f284b882500e5
levelopers <youcancallmeallen@gmail.com>
2019-01-10 00:00:25 -0500

    deploy heroku

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import  './header.sass'
 import styles from './header.module.sass'
+import $ from 'jquery'
+import './header.sass'
 export default function header(props) {
   return (
     <div className={styles.outbox}>
@@ -9,19 +10,39 @@ export default function header(props) {
         <Link to="/">Dongsheng Yin</Link>
       </div>
       <div className={styles.header_nav}>
-        <div>
+        <div className="header_nav">
           <Link to="/">Home</Link>
         </div>
-        <div>
+        <div className="header_nav">
           <Link to="/projects">Projects</Link>
         </div>
-        <div>
+        <div className="header_nav">
           <Link to="/about">About</Link>
         </div>
-        <div className={styles.contact}>
+        <div className={styles.contact} className="header_nav">
           Contact
         </div>
       </div>
     </div>
   )
 }
+// $(function() {  
+//   $('.header_nav')
+//     .on('mouseenter', function(e) {
+//       console.log(111);
+      
+// 			var parentOffset = $(this).offset(),
+//       		relX = e.pageX - parentOffset.left,
+//           relY = e.pageY - parentOffset.top;
+//           console.log(relX,relY);
+          
+// 			$(this).css({background:`rgb(${relX},${relY})`})
+//     })
+//     // .on('mouseout', function(e) {
+// 		// 	var parentOffset = $(this).offset(),
+//     //   		relX = e.pageX - parentOffset.left,
+//     //   		relY = e.pageY - parentOffset.top;
+//     // 	$(this).css({top:relY, left:relX})
+//     // });
+// });
+
